5b6876bce84bf6fdaca0c35a84c95f2135c068f0
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-03-02 17:18:21 +0000

    add rating notice

--- includes/classes/class-wpum-notices.php
+++ includes/classes/class-wpum-notices.php
@@ -34,6 +34,23 @@ class WPUM_Admin_Notices {
 			WPUM()->notices->register_notice( 'wpum_activated', 'success', $activation_message );
 		}
 
+		// Display a notice asking the user to leave a rating after 14 days.
+		$install_date = get_option( 'wpum_activation_date' );
+		$past_date    = strtotime( '-14 days' );
+
+		if ( $install_date && $past_date >= $install_date ) {
+			$url_rate     = 'http://wordpress.org/support/view/plugin-reviews/wp-user-manager?filter=5#postform';
+			$current_user = wp_get_current_user();
+			$current_user = $current_user->display_name;
+			$rating_message = sprintf(
+				__( "Hey %s, looks like you've been using the %s plugin for some time now - that's awesome! <br/> Could you please give it a review on wordpress.org? Just to help us spread the word and boost our motivation :) <br/> <br/><a href='%s' class='button button-primary' target='_blank'>Yes, you deserve it!</a>", 'wpum' ),
+				$current_user,
+				'<b>WP User Manager</b>',
+				$url_rate
+			);
+			WPUM()->notices->register_notice( 'wpum_rating', 'success', $rating_message );
+		}
+
 	}
 
 }
