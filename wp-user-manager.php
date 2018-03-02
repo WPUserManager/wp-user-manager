9b77c5bac2a9d86b263291a239f9becae2bc1e90
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-03-02 11:30:19 +0000

    scaffold v2

---
+++ wp-user-manager.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WP User Manager
+Plugin URI:  https://wpusermanager.com
+Description: Create customized user profiles and easily add custom user registration, login and password recovery forms to your WordPress website. WP User Manager is the best solution to manage your users.
+Version: 2.0.0
+Author:      Alessandro Tesoro
+Author URI:  https://wpusermanager.com
+License:     GPLv3+
+Text Domain: wpum
+Domain Path: /languages
+*/
+
+/**
+ * WP User Manager.
+ *
+ * Copyright (c) 2018 Alessandro Tesoro
+ *
+ * WP User Manager. is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * WP User Manager. is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * @author     Alessandro Tesoro
+ * @version    2.0.0
+ * @copyright  (c) 2018 Alessandro Tesoro
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU LESSER GENERAL PUBLIC LICENSE
+ * @package    wp-user-manager
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
