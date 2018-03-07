963fb38095cfa70c569afa521f68488cff928096
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-03-07 18:03:00 +0000

    styling for form and action links

--- templates/action-links.php
+++ templates/action-links.php
@@ -22,7 +22,7 @@ if( isset( $data->scalar ) ) {
 
 ?>
 
-<ul class="wpum-action-link">
+<ul class="wpum-action-links">
 	<?php if( isset( $data->login_link ) && $data->login_link == 'yes' ) : ?>
 	<li>
 		<?php echo apply_filters( 'wpum_login_link_label', sprintf( __( 'Already have an account? <a href="%s">Sign In &raquo;</a>', 'wpum' ), esc_url( get_permalink( wpum_get_core_page_id( 'login_page' ) ) ) ) ); ?>
