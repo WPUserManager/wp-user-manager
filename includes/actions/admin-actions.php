f693a2fa02acd874e989b200555923f6c1111963
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-03-18 18:32:53 +0000

    Add WPUM specific admin bar links.

--- includes/actions/admin-actions.php
+++ includes/actions/admin-actions.php
@@ -26,3 +26,41 @@ function wpum_delete_pages_transient( $post_id ) {
 
 }
 add_action( 'save_post_page', 'wpum_delete_pages_transient' );
+
+/**
+ * Add WPUM specific admin bar links.
+ *
+ * @param object $wp_admin_bar
+ * @return void
+ */
+function wpum_admin_bar_menu( $wp_admin_bar ) {
+
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	$args = array(
+		'id'    => 'wpum_node',
+		'href'  => admin_url( 'users.php' ),
+		'title' => '<span class="ab-icon dashicons dashicons-admin-users" style="margin-top:2px"></span>' . esc_html__( ' Users' ),
+	);
+	$wp_admin_bar->add_node( $args );
+
+	$args = array(
+		'id'     => 'wpum_emails',
+		'href'   => admin_url( 'users.php?page=wpum-emails' ),
+		'title'  => esc_html__( 'Emails' ),
+		'parent' => 'wpum_node',
+	);
+	$wp_admin_bar->add_node( $args );
+
+	$args = array(
+		'id'     => 'wpum_settings',
+		'href'   => admin_url( 'users.php?page=wpum-settings' ),
+		'title'  => esc_html__( 'Settings' ),
+		'parent' => 'wpum_node',
+	);
+	$wp_admin_bar->add_node( $args );
+
+}
+add_action( 'admin_bar_menu', 'wpum_admin_bar_menu', 100 );
