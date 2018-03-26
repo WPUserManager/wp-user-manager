abfb9acd6e77c45be0b94858bb1f2a81c51912c0
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-03-26 15:41:17 +0100

    prototype registration form

---
+++ includes/forms/class-wpum-form-registration.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Handles the WPUM registration form.
+ *
+ * @package     wp-user-manager
+ * @copyright   Copyright (c) 2018, Alessandro Tesoro
+ * @license     https://opensource.org/licenses/GPL-3.0 GNU Public License
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WPUM_Form_Register extends WPUM_Form {
+
+	/**
+	 * Form name.
+	 *
+	 * @var string
+	 */
+	public $form_name = 'registration';
+
+	/**
+	 * Determine if there's a referrer.
+	 *
+	 * @var mixed
+	 */
+	protected $referrer;
+
+	/**
+	 * Stores static instance of class.
+	 *
+	 * @access protected
+	 * @var WPUM_Form_Register The single instance of the class
+	 */
+	protected static $_instance = null;
+
+	/**
+	 * Returns static instance of class.
+	 *
+	 * @return self
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'wp', array( $this, 'process' ) );
+
+		$this->steps  = (array) apply_filters( 'registration_steps', array(
+			'submit' => array(
+				'name'     => esc_html__( 'Registration Details' ),
+				'view'     => array( $this, 'submit' ),
+				'handler'  => array( $this, 'submit_handler' ),
+				'priority' => 10
+			),
+			'done' => array(
+				'name'     => esc_html__( 'Done' ),
+				'view'     => false,
+				'handler'  => array( $this, 'done' ),
+				'priority' => 30
+			)
+		) );
+
+		uasort( $this->steps, array( $this, 'sort_by_priority' ) );
+
+		if ( isset( $_POST['step'] ) ) {
+			$this->step = is_numeric( $_POST['step'] ) ? max( absint( $_POST['step'] ), 0 ) : array_search( $_POST['step'], array_keys( $this->steps ) );
+		} elseif ( ! empty( $_GET['step'] ) ) {
+			$this->step = is_numeric( $_GET['step'] ) ? max( absint( $_GET['step'] ), 0 ) : array_search( $_GET['step'], array_keys( $this->steps ) );
+		}
+
+	}
+
+	/**
+	 * Initializes the fields used in the form.
+	 */
+	public function init_fields() {
+		if ( $this->fields ) {
+			return;
+		}
+
+		$this->fields = wpum_get_registration_fields();
+
+	}
+
+}
