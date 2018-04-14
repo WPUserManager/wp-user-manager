653398c7699136742edf505ec9ef24dca2188177
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-04-14 17:28:08 +0100

    add url field type output

---
+++ templates/form-fields/url-field.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the url field.
+ *
+ * This template can be overridden by copying it to yourtheme/wpum/form-fields/url-field.php
+ *
+ * HOWEVER, on occasion WPUM will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @version 1.0.0
+ */
+
+ // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+?>
+
+<input
+	type="url"
+	class="input-text"
+	name="<?php echo esc_attr( isset( $data->name ) ? $data->name : $data->key ); ?>"
+	<?php if ( isset( $data->autocomplete ) && false === $data->autocomplete ) { echo ' autocomplete="off"'; } ?>
+	id="<?php echo esc_attr( $data->key ); ?>"
+	placeholder="<?php echo empty( $data->placeholder ) ? '' : esc_attr( $data->placeholder ); ?>"
+	value="<?php echo isset( $data->value ) ? esc_attr( $data->value ) : ''; ?>"
+	maxlength="<?php echo ! empty( $data->maxlength ) ? $data->maxlength : ''; ?>"
+	<?php if ( ! empty( $data->required ) ) echo 'required'; ?>
+/>
+<?php if ( ! empty( $data->description ) ) : ?><small class="description"><?php echo $data->description; ?></small><?php endif; ?>
