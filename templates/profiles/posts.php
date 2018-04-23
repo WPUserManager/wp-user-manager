7d7d9d5ee997349e8a5ef275a299ec7d5b1c6bdf
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-04-23 15:23:59 +0100

    style the pagination

--- templates/profiles/posts.php
+++ templates/profiles/posts.php
@@ -42,23 +42,25 @@ $the_query = wpum_get_posts_for_profile( $data->user->ID );
 
 		<?php endwhile; ?>
 
-		<?php
-			echo paginate_links( array(
-				'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
-				'total'        => $the_query->max_num_pages,
-				'current'      => max( 1, get_query_var( 'paged' ) ),
-				'format'       => '?paged=%#%',
-				'show_all'     => false,
-				'type'         => 'plain',
-				'end_size'     => 2,
-				'mid_size'     => 1,
-				'prev_next'    => true,
-				'prev_text'    => sprintf( '<i></i> %1$s', esc_html__( 'Newer Posts' ) ),
-				'next_text'    => sprintf( '%1$s <i></i>', esc_html__( 'Older Posts' ) ),
-				'add_args'     => false,
-				'add_fragment' => '',
-			) );
-		?>
+		<div id="profile-pagination">
+			<?php
+				echo paginate_links( array(
+					'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+					'total'        => $the_query->max_num_pages,
+					'current'      => max( 1, get_query_var( 'paged' ) ),
+					'format'       => '?paged=%#%',
+					'show_all'     => false,
+					'type'         => 'plain',
+					'end_size'     => 2,
+					'mid_size'     => 1,
+					'prev_next'    => true,
+					'prev_text'    => sprintf( '<i></i> %1$s', esc_html__( 'Newer Posts' ) ),
+					'next_text'    => sprintf( '%1$s <i></i>', esc_html__( 'Older Posts' ) ),
+					'add_args'     => false,
+					'add_fragment' => '',
+				) );
+			?>
+		</div>
 
 		<?php wp_reset_postdata(); ?>
 
