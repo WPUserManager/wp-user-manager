a23450f25cfc829fb22883bc719a9a9898b82a4f
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-05-03 15:41:07 +0100

    add, update, delete methods for search fields

--- includes/wpum-database/class-wpum-db-search-fields.php
+++ includes/wpum-database/class-wpum-db-search-fields.php
@@ -59,4 +59,92 @@ class WPUM_DB_Search_Fields extends WPUM_DB {
 		);
 	}
 
+	/**
+	 * Insert a new field.
+	 *
+	 * @access public
+	 * @param array $data
+	 *
+	 * @return int ID of the inserted field.
+	 */
+	public function insert( $data, $type = '' ) {
+		$result = parent::insert( $data, $type );
+
+		if ( $result ) {
+			$this->set_last_changed();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Update a field.
+	 *
+	 * @access public
+	 * @param int   $row_id field ID.
+	 * @param array $data
+	 * @param mixed string|array $where Where clause to filter update.
+	 *
+	 * @return  bool
+	 */
+	public function update( $row_id, $data = array(), $where = '' ) {
+		$result = parent::update( $row_id, $data, $where );
+
+		if ( $result ) {
+			$this->set_last_changed();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Delete field.
+	 *
+	 * @access public
+	 * @param int $row_id ID of the field to delete.
+	 * @return bool True if deletion was successful, false otherwise.
+	 */
+	public function delete( $row_id = 0 ) {
+		if ( empty( $row_id ) ) {
+			return false;
+		}
+
+		$result = parent::delete( $row_id );
+
+		if ( $result ) {
+			$this->set_last_changed();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Sets the last_changed cache key for fields.
+	 *
+	 * @access public
+	 */
+	public function set_last_changed() {
+		wp_cache_set( 'last_changed', microtime(), $this->cache_group );
+	}
+
+	/**
+	 * Retrieves the value of the last_changed cache key for fields.
+	 *
+	 * @access public
+	 * @return string Value of the last_changed cache key for fields.
+	 */
+	public function get_last_changed() {
+		if ( function_exists( 'wp_cache_get_last_changed' ) ) {
+			return wp_cache_get_last_changed( $this->cache_group );
+		}
+
+		$last_changed = wp_cache_get( 'last_changed', $this->cache_group );
+		if ( ! $last_changed ) {
+			$last_changed = microtime();
+			wp_cache_set( 'last_changed', $last_changed, $this->cache_group );
+		}
+
+		return $last_changed;
+	}
+
 }
