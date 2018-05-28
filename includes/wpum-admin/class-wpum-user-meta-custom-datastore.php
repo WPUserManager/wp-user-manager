f810dfc3a77b7fa5a81eb30c147595e8771ec939
Alessandro Tesoro <mythemesdepot@gmail.com>
2018-05-28 15:01:54 +0100

    change carbon fields load method parameter

--- includes/wpum-admin/class-wpum-user-meta-custom-datastore.php
+++ includes/wpum-admin/class-wpum-user-meta-custom-datastore.php
@@ -16,69 +16,68 @@ use Carbon_Fields\Helper\Helper;
  */
 class WPUM_User_Meta_Custom_Datastore extends Datastore {
 
-    /**
-     * Initialization tasks for concrete datastores.
-     **/
-    public function init() {
+	/**
+	 * Initialization tasks for concrete datastores.
+	 **/
+	public function init() {
 
-    }
+	}
 
-    protected function get_key_for_field( Field $field ) {
-        $key = $field->get_base_name();
-        return $key;
-    }
+	protected function get_key_for_field( Field $field ) {
+		$key = $field->get_base_name();
+		return $key;
+	}
 
-    /**
-     * Save a single key-value pair to the database with autoload
-     *
-     * @param string $key
-     * @param string $value
-     * @param bool $autoload
-     */
-    protected function save_key_value_pair_with_autoload( $key, $value, $autoload ) {
-        update_user_meta( $this->object_id, $key, $value);
-    }
+	/**
+	 * Save a single key-value pair to the database with autoload
+	 *
+	 * @param string $key
+	 * @param string $value
+	 * @param bool $autoload
+	 */
+	protected function save_key_value_pair_with_autoload( $key, $value, $autoload ) {
+		update_user_meta( $this->object_id, $key, $value );
+	}
 
-    /**
-     * Load the field value(s)
-     *
-     * @param Field $field The field to load value(s) in.
-     * @return array
-     */
-    public function load( Field $field ) {
-        $key = $this->get_key_for_field( $field );
-        $value = get_user_meta( $this->object_id, $key, null );
+	/**
+	 * Load the field value(s)
+	 *
+	 * @param Field $field The field to load value(s) in.
+	 * @return array
+	 */
+	public function load( Field $field ) {
+		$key   = $this->get_key_for_field( $field );
+		$value = get_user_meta( $this->object_id, $key, true );
+		return $value;
+	}
 
-        return $value;
-    }
+	/**
+	 * Save the field value(s)
+	 *
+	 * @param Field $field The field to save.
+	 */
+	public function save( Field $field ) {
+		if ( ! empty( $field->get_hierarchy() ) ) {
+			return; // only applicable to root fields
+		}
+		$key   = $this->get_key_for_field( $field );
+		$value = $field->get_value();
+		if ( is_a( $field, '\\Carbon_Fields\\Field\\Complex_Field' ) ) {
+			$value = $field->get_value_tree();
+		}
+		$this->save_key_value_pair_with_autoload( $key, $value, $field->get_autoload() );
+	}
 
-    /**
-     * Save the field value(s)
-     *
-     * @param Field $field The field to save.
-     */
-    public function save( Field $field ) {
-        if ( ! empty( $field->get_hierarchy() ) ) {
-            return; // only applicable to root fields
-        }
-        $key = $this->get_key_for_field( $field );
-        $value = $field->get_value();
-        if ( is_a( $field, '\\Carbon_Fields\\Field\\Complex_Field' ) ) {
-            $value = $field->get_value_tree();
-        }
-        $this->save_key_value_pair_with_autoload( $key, $value, $field->get_autoload() );
-    }
-
-    /**
-     * Delete the field value(s)
-     *
-     * @param Field $field The field to delete.
-     */
-    public function delete( Field $field ) {
-        if ( ! empty( $field->get_hierarchy() ) ) {
-            return; // only applicable to root fields
-        }
-        $key = $this->get_key_for_field( $field );
-        delete_user_meta( $this->get_object_id(), $key );
-    }
+	/**
+	 * Delete the field value(s)
+	 *
+	 * @param Field $field The field to delete.
+	 */
+	public function delete( Field $field ) {
+		if ( ! empty( $field->get_hierarchy() ) ) {
+			return; // only applicable to root fields
+		}
+		$key = $this->get_key_for_field( $field );
+		delete_user_meta( $this->get_object_id(), $key );
+	}
 }
