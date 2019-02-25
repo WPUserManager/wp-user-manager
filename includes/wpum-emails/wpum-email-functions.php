e15054bc19a611f05abd2840f43a5d2e7c8e7971
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-25 09:57:32 +0000

    fix: wrongly formatted urls in emails when using third party providers

--- includes/wpum-emails/wpum-email-functions.php
+++ includes/wpum-emails/wpum-email-functions.php
@@ -8,7 +8,9 @@
  * @since       1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Retrieve the list of available email templates.
@@ -25,10 +27,10 @@ function wpum_get_email_templates() {
  * @return string
  */
 function wpum_get_emails_tags_list() {
-	$list = '';
+	$list       = '';
 	$email_tags = WPUM()->emails->get_tags();
-	if( count( $email_tags ) > 0 ) {
-		foreach( $email_tags as $email_tag ) {
+	if ( count( $email_tags ) > 0 ) {
+		foreach ( $email_tags as $email_tag ) {
 			$list .= '{' . $email_tag['tag'] . '} - ' . $email_tag['description'] . '<br />';
 		}
 	}
@@ -66,7 +68,7 @@ function wpum_email_tag_username( $user_id ) {
 	$user     = get_user_by( 'id', $user_id );
 	$username = '';
 
-	if( $user instanceof WP_User ) {
+	if ( $user instanceof WP_User ) {
 		$username = $user->data->user_login;
 	}
 
@@ -81,10 +83,10 @@ function wpum_email_tag_username( $user_id ) {
  */
 function wpum_email_tag_email( $user_id ) {
 
-	$user     = get_user_by( 'id', $user_id );
+	$user  = get_user_by( 'id', $user_id );
 	$email = '';
 
-	if( $user instanceof WP_User ) {
+	if ( $user instanceof WP_User ) {
 		$email = $user->data->user_email;
 	}
 
@@ -130,8 +132,8 @@ function wpum_email_tag_login_page_url( $user_id = false ) {
 
 	$url = $login_page_url;
 
-	if( wpum_get_option( 'email_template' ) !== 'none' ) {
-		$url = '<a href="'. esc_url( $login_page_url ) .'">' . esc_html( $login_page_url ) . '</a>';
+	if ( wpum_get_option( 'email_template' ) !== 'none' ) {
+		$url = '<a href="' . htmlspecialchars( $login_page_url ) . '">' . esc_html( $login_page_url ) . '</a>';
 	}
 
 	return $url;
@@ -151,18 +153,21 @@ function wpum_email_tag_password_recovery_url( $user_id, $password_reset_key ) {
 
 	$reset_page = wpum_get_core_page_id( 'password' );
 	$reset_page = get_permalink( $reset_page );
-	$reset_page = add_query_arg( [
-		'user_id' => $user_id,
-		'key'     => $password_reset_key,
-		'step'    => 'reset'
-	], $reset_page );
+	$reset_page = add_query_arg(
+		[
+			'user_id' => $user_id,
+			'key'     => $password_reset_key,
+			'step'    => 'reset',
+		],
+		$reset_page
+	);
 
 	$link_color = apply_filters( 'wpum_email_tag_password_recovery_url_color', '#000' );
 
 	$output = $reset_page;
 
-	if( wpum_get_option( 'email_template' ) !== 'none' ) {
-		$output = '<a href="' . esc_url( $reset_page ) . '" style="color:' . $link_color . '">' . esc_html( $reset_page ) . '</a>';
+	if ( wpum_get_option( 'email_template' ) !== 'none' ) {
+		$output = '<a href="' . htmlspecialchars( $reset_page ) . '" style="color:' . $link_color . '">' . esc_html( $reset_page ) . '</a>';
 	}
 
 	return $output;
@@ -177,17 +182,17 @@ function wpum_get_registered_emails() {
 
 	$emails = [
 		'registration_confirmation' => [
-			'status'            => 'active',
-			'name'              => esc_html__( 'Registration confirmation', 'wp-user-manager' ),
-			'description'       => esc_html__( 'This is the email that is sent to the user upon successful registration.', 'wp-user-manager' ),
-			'recipient'         => esc_html__( 'User\'s email.', 'wp-user-manager' ),
+			'status'      => 'active',
+			'name'        => esc_html__( 'Registration confirmation', 'wp-user-manager' ),
+			'description' => esc_html__( 'This is the email that is sent to the user upon successful registration.', 'wp-user-manager' ),
+			'recipient'   => esc_html__( 'User\'s email.', 'wp-user-manager' ),
 		],
 		'password_recovery_request' => [
-			'status'            => 'active',
-			'name'              => esc_html__( 'Password recovery request', 'wp-user-manager' ),
-			'description'       => esc_html__( 'This is the email that is sent to the visitor upon password reset request.', 'wp-user-manager' ),
-			'recipient'         => esc_html__( 'Email address of the requested user.', 'wp-user-manager' ),
-		]
+			'status'      => 'active',
+			'name'        => esc_html__( 'Password recovery request', 'wp-user-manager' ),
+			'description' => esc_html__( 'This is the email that is sent to the visitor upon password reset request.', 'wp-user-manager' ),
+			'recipient'   => esc_html__( 'Email address of the requested user.', 'wp-user-manager' ),
+		],
 	];
 
 	return apply_filters( 'wpum_registered_emails', $emails );
@@ -203,16 +208,16 @@ function wpum_get_registered_emails() {
  */
 function wpum_get_email_field( $email_id = false, $field_id = false ) {
 
-	if( ! $email_id || ! $field_id ) {
+	if ( ! $email_id || ! $field_id ) {
 		return false;
 	}
 
-	$output = false;
+	$output       = false;
 	$stored_email = get_option( 'wpum_email', false );
 
-	if( ! empty( $stored_email ) && is_array( $stored_email ) && array_key_exists( $email_id, $stored_email ) ) {
+	if ( ! empty( $stored_email ) && is_array( $stored_email ) && array_key_exists( $email_id, $stored_email ) ) {
 		$found_email = $stored_email[ $email_id ];
-		if( array_key_exists( $field_id, $found_email ) ) {
+		if ( array_key_exists( $field_id, $found_email ) ) {
 			$output = $found_email[ $field_id ];
 		}
 	}
@@ -231,13 +236,13 @@ function wpum_get_email( $email_id = false ) {
 
 	$email = false;
 
-	if( ! $email_id ) {
+	if ( ! $email_id ) {
 		return false;
 	}
 
 	$emails = get_option( 'wpum_email', false );
 
-	if( array_key_exists( $email_id, $emails ) && is_array( $emails[ $email_id ] ) ) {
+	if ( array_key_exists( $email_id, $emails ) && is_array( $emails[ $email_id ] ) ) {
 		$email = $emails[ $email_id ];
 	}
 
@@ -249,7 +254,7 @@ function wpum_get_email( $email_id = false ) {
  * Disable the email notification sent to the admin when a user changes the password.
  */
 if ( wpum_get_option( 'disable_admin_password_recovery_email' ) && ! function_exists( 'wp_password_change_notification' ) ) {
-    function wp_password_change_notification( $user ) {
-        return;
-    }
+	function wp_password_change_notification( $user ) {
+		return;
+	}
 }
