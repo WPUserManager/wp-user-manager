ba9d9cb5f4d151b02b6442bdeaa857fc08209d9e
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-02-25 11:07:24 +0000

    tweak: added automatic data installation fixer

--- includes/wpum-admin/class-wpum-notices.php
+++ includes/wpum-admin/class-wpum-notices.php
@@ -5,10 +5,12 @@
  * @package     wp-user-manager
  * @copyright   Copyright (c) 2018, Alessandro Tesoro
  * @license     https://opensource.org/licenses/GPL-3.0 GNU Public License
-*/
+ */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * WPUM Admin Notices class that registers all the notices that need to be displayed.
@@ -20,6 +22,7 @@ class WPUM_Admin_Notices {
 	 */
 	public function __construct() {
 		add_action( 'admin_init', [ $this, 'register_notices' ] );
+		add_action( 'admin_init', [ $this, 'fix_data_installation' ] );
 	}
 
 	/**
@@ -34,11 +37,11 @@ class WPUM_Admin_Notices {
 		$past_date    = strtotime( '-14 days' );
 
 		if ( $install_date && $past_date >= $install_date ) {
-			$url_rate     = 'http://wordpress.org/support/view/plugin-reviews/wp-user-manager?filter=5#postform';
-			$current_user = wp_get_current_user();
-			$current_user = $current_user->display_name;
+			$url_rate       = 'http://wordpress.org/support/view/plugin-reviews/wp-user-manager?filter=5#postform';
+			$current_user   = wp_get_current_user();
+			$current_user   = $current_user->display_name;
 			$rating_message = sprintf(
-				__( "Hey %s, looks like you've been using the %s plugin for some time now - that's awesome! <br/> Could you please give it a review on wordpress.org? Just to help us spread the word and boost our motivation :) <br/> <br/><a href='%s' class='button button-primary' target='_blank'>Yes, you deserve it!</a>", 'wp-user-manager' ),
+				__( "Hey %1\$s, looks like you've been using the %2\$s plugin for some time now - that's awesome! <br/> Could you please give it a review on wordpress.org? Just to help us spread the word and boost our motivation :) <br/> <br/><a href='%3\$s' class='button button-primary' target='_blank'>Yes, you deserve it!</a>", 'wp-user-manager' ),
 				$current_user,
 				'<b>WP User Manager</b>',
 				$url_rate
@@ -46,8 +49,115 @@ class WPUM_Admin_Notices {
 			WPUM()->notices->register_notice( 'wpum_rating', 'success', $rating_message );
 		}
 
+		$option_check = get_option( 'wpum_data_installation_fix_check', false );
+
+		if ( $this->field_groups_are_empty() && $this->fields_are_empty() && $this->registration_forms_are_empty() && ! $option_check ) {
+
+			$url = add_query_arg( [ 'wpum_fix_installation_data' => true ], admin_url() );
+
+			$btn           = '<a href="' . esc_url( $url ) . '" class="button-primary">' . esc_html__( 'Fix data installation' ) . '</a>';
+			$error_message = esc_html__( 'It looks like WP User Manager failed to install it\'s default data. To fix the issue please click the button below.' ) . '</br><br/>' . $btn;
+
+			WPUM()->notices->register_notice( 'wpum_fix_installation', 'error', $error_message );
+
+		} else {
+
+			update_option( 'wpum_data_installation_fix_check', true );
+
+		}
+
+	}
+
+	/**
+	 * Verify groups are empty.
+	 *
+	 * @return boolean
+	 */
+	private function field_groups_are_empty() {
+
+		$fields_groups = WPUM()->fields_groups->get_groups();
+
+		if ( empty( $fields_groups ) ) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Verify fields are empty.
+	 *
+	 * @return boolean
+	 */
+	private function fields_are_empty() {
+
+		global $wpdb;
+
+		$empty = false;
+
+		if ( $this->field_groups_are_empty() ) {
+			$results = $wpdb->get_results( '"SELECT COUNT(*) FROM {$wpdb->prefix}wpum_fields WHERE id IS NOT NULL"' );
+			if ( is_null( $results ) || ! empty( $wpdb->last_error ) ) {
+				return true;
+			}
+		}
+
+		return $empty;
+
+	}
+
+	/**
+	 * Verify there are no registration forms.
+	 *
+	 * @return boolean
+	 */
+	private function registration_forms_are_empty() {
+
+		$forms = WPUM()->registration_forms->get_forms();
+
+		if ( empty( $forms ) ) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Fix data installation if something went wrong.
+	 *
+	 * @return void
+	 */
+	public function fix_data_installation() {
+
+		if ( current_user_can( 'manage_options' ) && isset( $_GET['wpum_fix_installation_data'] ) && ! get_option( 'wpum_data_installation_fix_check', false ) ) {
+			delete_option( 'wpum_setup_is_complete' );
+			delete_option( 'wpum_version_upgraded_from' );
+			delete_option( 'wpum_version_upgraded_from' );
+
+			wpum_install_default_field_group();
+			wpum_install_fields();
+			wpum_install_cover_image_field();
+			wpum_setup_default_custom_search_fields();
+			wpum_install_registration_form();
+
+			update_option( 'wpum_setup_is_complete', true );
+
+			$current_version = get_option( 'wpum_version' );
+			if ( $current_version ) {
+				update_option( 'wpum_version_upgraded_from', $current_version );
+			}
+
+			update_option( 'wpum_data_installation_fix_check', true );
+
+			$message = sprintf( __( 'WP User Manager has installed default data. You can go back to your <a href="%s">admin panel.</a>' ), admin_url() );
+
+			wp_die( $message );
+
+		}
 	}
 
 }
 
-new WPUM_Admin_Notices;
+new WPUM_Admin_Notices();
