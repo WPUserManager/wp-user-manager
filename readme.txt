37eb9afd387453f6d0deb697dce855ca73ff1acd
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-08-18 12:41:08 +0100

    version bump

--- readme.txt
+++ readme.txt
@@ -8,7 +8,7 @@ Tested up to: 5.2
 Requires PHP: 5.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Stable tag: 2.1.7
+Stable tag: 2.1.8
 
 The most customizable profiles & community builder WordPress plugin with front-end login, registration, profile customization and content restriction.
 
--- wp-user-manager.php
+++ wp-user-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: WP User Manager
 Plugin URI:  https: //wpusermanager.com
 Description: Beautifully simple user profile directories with frontend login, registration and account customization. WP User Manager is the best solution to manage your community and your users for WordPress.
-Version:     2.1.7
+Version:     2.1.8
 Author:      Alessandro Tesoro
 Author URI:  https://wpusermanager.com
 License:     GPLv3+
@@ -408,7 +408,7 @@ if ( ! class_exists( 'WP_User_Manager' ) ) :
 
 			// Plugin version.
 			if ( ! defined( 'WPUM_VERSION' ) ) {
-				define( 'WPUM_VERSION', '2.1.7' );
+				define( 'WPUM_VERSION', '2.1.8' );
 			}
 
 			// Plugin Folder Path.
