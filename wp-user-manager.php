484580d01c0ff137da481bfd9425f29307b8cf0f
Iain <iain@polevaultweb.com>
2020-05-27 23:12:16 +0100

    Add ACF metabox to the field group edit screen

--- wp-user-manager.php
+++ wp-user-manager.php
@@ -260,6 +260,7 @@ if ( ! class_exists( 'WP_User_Manager' ) ) :
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-admin/class-wpum-addons-page.php';
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-admin/class-wpum-notices.php';
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-admin/class-wpum-user-table.php';
+				require_once WPUM_PLUGIN_DIR . 'includes/wpum-admin/class-wpum-acf.php';
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-admin/class-wpum-permalinks-settings.php';
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-fields/class-wpum-fields-editor.php';
 				require_once WPUM_PLUGIN_DIR . 'includes/wpum-forms/class-wpum-registration-forms-editor.php';
