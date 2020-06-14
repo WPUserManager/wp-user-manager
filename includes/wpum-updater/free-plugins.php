b535b0878f0dfce7c56cc3b3dce0cb639bc0fddb
Iain <iain@polevaultweb.com>
2020-06-14 07:56:38 +0100

    Check for free plugin updates even if not activated.

--- includes/wpum-updater/free-plugins.php
+++ includes/wpum-updater/free-plugins.php
@@ -15,8 +15,9 @@ add_action( 'admin_init', 'wpum_free_plugins_auto_updater', 0 );
 function wpum_free_plugins_auto_updater() {
 	$api_url = 'https://wpusermanager.com';
 
-	if ( defined( 'WPUMDA_PLUGIN_FILE' ) ) {
-		new WPUM_EDD_SL_Plugin_Updater( $api_url, WPUMDA_PLUGIN_FILE, array(
+	$delete_account = WP_PLUGIN_DIR . '/wpum-delete-account/wpum-delete-account.php';
+	if ( defined( 'WPUMDA_PLUGIN_FILE' ) || file_exists( $delete_account ) ) {
+		new WPUM_EDD_SL_Plugin_Updater( $api_url, $delete_account, array(
 			'version'   => WPUMDA_VERSION,
 			'item_id'   => 25531,
 			'license'   => '72fe94f839964c6210607d11025ab599',
@@ -26,8 +27,9 @@ function wpum_free_plugins_auto_updater() {
 		) );
 	}
 
-	if ( defined( 'WPUMPD_PLUGIN_FILE' ) ) {
-		new WPUM_EDD_SL_Plugin_Updater( $api_url, WPUMPD_PLUGIN_FILE, array(
+	$personal_data = WP_PLUGIN_DIR . '/wpum-personal-data/wpum-personal-data.php';
+	if ( defined( 'WPUMPD_PLUGIN_FILE' ) || file_exists( $personal_data ) ) {
+		new WPUM_EDD_SL_Plugin_Updater( $api_url, $personal_data, array(
 			'version'   => WPUMPD_VERSION,
 			'item_id'   => 25551,
 			'license'   => '72fe94f839964c6210607d11025ab599',
