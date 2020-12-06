dba64bf59f4170d1a8b4011752baff619ad950c0
Iain <iain@polevaultweb.com>
2020-12-06 22:08:54 +0000

    Add phpstan config and bootstrap

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace {
+	define( 'DOING_AJAX', false );
+	define( 'WPUM_PLUGIN_URL', '' );
+	define( 'WPUM_VERSION', '' );
+	define( 'WPUM_PLUGIN_FILE', '' );
+	define( 'WPUM_PLUGIN_DIR', '' );
+}
