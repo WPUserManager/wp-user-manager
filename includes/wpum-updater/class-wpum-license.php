5b85f04e8935d96ffa085788d7f4701ae16b93d2
Iain <iain@polevaultweb.com>
2020-12-11 11:09:24 +0000

    Make the plugin table update notice row for addons more like the WordPress default, and integrate with auto updates column.

--- includes/wpum-updater/class-wpum-license.php
+++ includes/wpum-updater/class-wpum-license.php
@@ -394,24 +394,30 @@ class WPUM_License {
 	 * Add a notice when the license has not been activated yet.
 	 *
 	 * @param string $plugin_file
-	 * @param array $plugin_data
+	 * @param array  $plugin_data
 	 * @param string $status
-	 * @return void
 	 */
 	public function plugin_page_notices( $plugin_file, $plugin_data, $status ) {
-
-		// Bailout.
 		if ( get_option( 'wpum_' . $this->item_shortname . '_license_active' ) && get_option( 'wpum_' . $this->item_shortname . '_license_active' ) == 'valid' ) {
-			return false;
+			return;
 		}
 
-		$update_notice_wrap = '<tr class="wpum-addon-notice-tr active"><td colspan="3" class="colspanchange"><div class="notice inline notice-error notice-alt wpum-invalid-license"><p><span class="dashicons dashicons-info" style="color:red;"></span> %s</p></div></td></tr>';
+		$colspan = wp_is_auto_update_enabled_for_type( 'plugin' ) ? 4 : 3;
+
+		$update_notice_wrap = '<tr class="plugin-update-tr wpum-addon-notice-tr active"><td colspan="' . $colspan . '" class="colspanchange plugin-update"><div class="notice inline notice-error notice-alt wpum-invalid-license"><p><span class="dashicons dashicons-info" style="color:red;"></span> %s</p></div></td></tr>';
 		$message            = $this->license_state_message();
 
 		if ( ! empty( $message['message'] ) ) {
 			echo sprintf( $update_notice_wrap, $message['message'] );
+			?>
+			<script type="application/javascript">
+				(function ($) {
+					$( document ).ready( function() {
+						$( 'tr[data-plugin="<?php echo $plugin_file; ?>"]').addClass('update');
+					} );
+				})( jQuery );
+			</script> <?php
 		}
-
 	}
 
 	/**
