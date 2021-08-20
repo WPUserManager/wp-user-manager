1a6b1961665f7323953541fb10da79c92a376308
Iain <iain@polevaultweb.com>
2021-08-20 09:27:35 +0100

    Filter typo

--- templates/user-overview.php
+++ templates/user-overview.php
@@ -32,7 +32,7 @@ $user = wp_get_current_user();
 			<ul>
 				<li>
 					<?php
-					$edit_account_text = apply_filters( 'wpun_profile_edit_account_text', 'Edit account', $user->data->ID ); ?>
+					$edit_account_text = apply_filters( 'wpum_profile_edit_account_text', 'Edit account', $user->data->ID ); ?>
 					<a href="<?php echo get_permalink( wpum_get_core_page_id( 'account' ) ); ?>"><?php echo esc_html( $edit_account_text ); ?></a>
 				</li>
 				<li>|</li>
