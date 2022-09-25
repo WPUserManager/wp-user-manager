c4d04c9f012dd05a531d731ee947db37923132aa
Iain <iain@polevaultweb.com>
2022-09-25 13:31:37 +0100

    Handle addons calling core dependency classes and functions

--- includes/class-wp-user-manager.php
+++ includes/class-wp-user-manager.php
@@ -191,10 +191,39 @@ if ( ! class_exists( 'WP_User_Manager' ) ) :
 		private function autoload() {
 			if ( file_exists( dirname( $this->plugin_file ) . '/vendor-dist/scoper-autoload.php' ) ) {
 				require_once dirname( $this->plugin_file ) . '/vendor-dist/scoper-autoload.php';
+				\spl_autoload_register( array( $this, 'ensure_addon_class_alias' ), true, true );
+				require_once dirname( $this->plugin_file ) . '/includes/functions-scoped.php ';
 			} else if ( file_exists( dirname( $this->plugin_file ) . '/vendor/autoload.php' ) ) {
 				require_once dirname( $this->plugin_file ) . '/vendor/autoload.php';
 				\spl_autoload_register( array( $this, 'ensure_class_alias' ), true, true );
 			}
+		}
+
+		/**
+		 * Ensure WPUM addon calls to core dependencies still work when calling unscoped versions
+		 *
+		 * @param string $class Class to create alias for.
+		 *
+		 * @return void
+		 */
+		public function ensure_addon_class_alias( $class ) {
+			if ( strpos( $class, 'WPUM\\' ) === 0 ) {
+				// Class is already scoped
+				return;
+			}
+
+			if ( class_exists( $class ) ) {
+				// Class exists already
+				return;
+			}
+
+			$scoped = 'WPUM\\' . $class;
+			if ( ! class_exists( $scoped ) ) {
+				// Scoped version of the class doesn't exist
+				return;
+			}
+
+			class_alias( $scoped, $class );
 		}
 
 		/**
