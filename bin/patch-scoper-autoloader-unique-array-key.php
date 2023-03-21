9283e7fe7b9497cfbbec10f59b0c8f54a5e420d8
Iain <iain@polevaultweb.com>
2023-03-21 18:36:43 +0000

    Fix build process for composer autoloader

--- bin/patch-scoper-autoloader-unique-array-key.php
+++ bin/patch-scoper-autoloader-unique-array-key.php
@@ -8,15 +8,17 @@
  */
 
 
-$scoper_path = './release/scoper/build/vendor/composer';
+parse_str( $argv[1], $args );
+
+$scoper_path = './release/' . $args['version'] . '/scoped/vendor/composer';
 
 $static_loader_path = $scoper_path . '/autoload_static.php';
 $static_loader      = file_get_contents( $static_loader_path );
-$static_loader      = \preg_replace( '/\'([A-Za-z0-9]*?)\' => __DIR__ \. (.*?)/', '\'a$1\' => __DIR__ . $2', $static_loader );
+$static_loader      = \preg_replace( '/\'([A-Za-z0-9]*?)\' => __DIR__ \. (.*?)/', '\'wpum$1\' => __DIR__ . $2', $static_loader );
 file_put_contents( $static_loader_path, $static_loader );
 
 
 $files_loader_path = $scoper_path . '/autoload_files.php';
 $files_loader      = file_get_contents( $files_loader_path );
-$files_loader      = \preg_replace( '/\'(.*?)\' => /', '\'a$1\' => ', $files_loader );
+$files_loader      = \preg_replace( '/\'(.*?)\' => /', '\'wpum$1\' => ', $files_loader );
 file_put_contents( $files_loader_path, $files_loader );
