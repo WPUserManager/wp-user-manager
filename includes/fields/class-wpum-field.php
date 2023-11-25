97a50b02fb142639118f9907a3ea23df543d35a5
Rey Calantaol <reygcalantaol@gmail.com>
2023-11-26 06:08:07 +0800

    Fix: Display Taxonomy Field data in user profiles
    Resolves https://github.com/WPUserManager/wpum-custom-fields/issues/85
    
    @polevaultweb

--- includes/fields/class-wpum-field.php
+++ includes/fields/class-wpum-field.php
@@ -637,7 +637,7 @@ class WPUM_Field {
 					$value = $user->data->user_email;
 					break;
 			}
-		} elseif ( strpos( $this->get_meta( 'user_meta_key' ), 'wpum_' ) === 0 ) {
+		} elseif ( strpos( $this->get_meta( 'user_meta_key' ), 'wpum_' ) === 0 && $this->get_type() !== 'taxonomy' ) {
 			$value = \WPUM\carbon_get_user_meta( $user_id, $this->get_meta( 'user_meta_key' ) );
 		} else {
 			$value = get_user_meta( $user_id, $this->get_meta( 'user_meta_key' ), true );
